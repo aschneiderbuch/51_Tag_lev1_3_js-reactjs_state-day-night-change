83ff11528f4047b4a3c13563958b0f9ac6d48ba8
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-22 20:10:09 +0100

    start

---
+++ src/htmlGerüst/HtmlGerüst.jsx
@@ -0,0 +1,26 @@
+
+
+
+
+// import css
+import "./HtmlGerüst.scss"
+
+
+
+
+
+// sfc
+const HtmlGerüst = () => {
+    return ( 
+
+<section>
+<h2>Hallo</h2>
+
+
+</section>
+
+
+     );
+}
+ 
+export default HtmlGerüst;
